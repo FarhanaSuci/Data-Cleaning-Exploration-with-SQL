cac3ea12dc94ba1e5ec6ddecc7a465c4b63b3055
unknown <farhanaaktersuci@gmail.com>
2025-10-23 23:10:48 +0600

    Add covid percentage

--- DataExplorationMS_SQL.sql
+++ DataExplorationMS_SQL.sql
@@ -2,14 +2,31 @@
  From SUCHI..CovidDeaths$
  order by 3,4;
 
- select *
- From SUCHI..CovidVaccinations$
- order by 3,4;
+ --select *
+ --From SUCHI..CovidVaccinations$
+ --order by 3,4;
 
+ --Select Data we are going to be using
+ 
+ Select location, date, total_cases,
+ new_cases, total_deaths, population
+ From SUCHI..CovidDeaths$
+ order by 1,2; 
 
+ --Looking at Total Cases vs Total Deaths
+  Select location,date,total_cases,
+  total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+  From SUCHI..CovidDeaths$
+  Where location like '%states%'
+  order by 1,2;
 
+  --Looking at Total Cases vs Population
+ --Shows what percentage of population got covid
 
-
-
+ Select location,date,population,total_cases,
+   (total_cases/population)*100 as CovidPercentage
+  From SUCHI..CovidDeaths$
+  Where location like '%states%'
+  order by 1,2;
 
 
