eea9629d45704ca78100e07f344ffe44f9f4f607
unknown <farhanaaktersuci@gmail.com>
2025-10-24 12:40:58 +0600

    Total population vs Vaccinations

--- DataExplorationMS_SQL.sql
+++ DataExplorationMS_SQL.sql
@@ -58,13 +58,33 @@
   order by TotalDeathCount desc;
 
   --Global numbers
-  Select date,population,total_cases,
-   (total_cases/population)*100 as PercentagePopulationInfected
+  Select SUM(new_cases) as total_cases,SUM(cast(new_deaths as int))
+   as total_deaths,SUM(cast(new_deaths as int))/SUM(
+   new_cases)*100 as DeathPercentage
   From SUCHI..CovidDeaths$
   --Where location like '%states%'
   Where continent is not null
-  GROUP BY date
+  --GROUP BY date
   order by 1,2;
+
+
+--Now  covid vaccinations dataset
+ select *
+ From SUCHI..CovidVaccinations$
+ order by 3,4;
+
+ --Now joining covid deaths and vaccinations datasets 
+ select *
+ From SUCHI..CovidVaccinations$ vac
+ JOIN
+ SUCHI..CovidDeaths$ death
+ ON vac.location = death.location;
+
+ --Looking at Total population VS Vaccinations 
+
+
+
+
 
 
 
